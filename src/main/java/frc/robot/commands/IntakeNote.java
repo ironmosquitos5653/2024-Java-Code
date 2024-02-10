d95eb0a79995b475f1c509d52319a83969e7b83f
Iron Mosquitos <119903571+ironmosquitos5653@users.noreply.github.com>
2024-02-10 13:39:25 -0600

    added amp commands

---
+++ src/main/java/frc/robot/commands/IntakeNote.java
@@ -0,0 +1,51 @@
+// Copyright (c) FIRST and other WPILib contributors.
+// Open Source Software; you can modify and/or share it under the terms of
+// the WPILib BSD license file in the root directory of this project.
+
+package frc.robot.commands;
+
+import edu.wpi.first.wpilibj.Timer;
+import edu.wpi.first.wpilibj2.command.Command;
+import frc.robot.subsystems.AmpSubsystem;
+import frc.robot.subsystems.IntakeSubsystem;
+
+public class IntakeNote extends Command {
+  /** Creates a new SuckNote. */
+private AmpSubsystem m_AmpSubsystem;
+private IntakeSubsystem m_IntakeSubsystem;
+private Timer timer;
+  public IntakeNote(AmpSubsystem ampSubsystem, IntakeSubsystem intakeSubsystem) {
+    m_AmpSubsystem = ampSubsystem;
+    m_IntakeSubsystem = intakeSubsystem;
+    addRequirements(ampSubsystem);
+    addRequirements(intakeSubsystem);
+  }
+
+  // Called when the command is initially scheduled.
+  @Override
+  public void initialize() {
+    timer = new Timer();
+    timer.reset();
+    timer.start();
+  }
+
+  // Called every time the scheduler runs while the command is scheduled.
+  @Override
+  public void execute() {
+    m_IntakeSubsystem.ampAdvance(.5);
+    m_AmpSubsystem.setSpeed(.5);
+  }
+
+  // Called once the command ends or is interrupted.
+  @Override
+  public void end(boolean interrupted) {
+    m_IntakeSubsystem.ampAdvance(0);
+    m_AmpSubsystem.setSpeed(0);
+  }
+
+  // Returns true when the command should end.
+  @Override
+  public boolean isFinished() {
+    return timer.hasElapsed(2);
+  }
+}
--- src/main/java/frc/robot/subsystems/AmpSubsystem.java
+++ src/main/java/frc/robot/subsystems/AmpSubsystem.java
@@ -4,25 +4,59 @@
 
 package frc.robot.subsystems;
 
+import com.revrobotics.AbsoluteEncoder;
 import com.revrobotics.CANSparkFlex;
 import com.revrobotics.CANSparkLowLevel.MotorType;
+import com.revrobotics.SparkAbsoluteEncoder.Type;
 import com.revrobotics.CANSparkMax;
 
+import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
 
 public class AmpSubsystem extends SubsystemBase {
 
   private static final int deviceIDamp = 17;
   private CANSparkMax amp;
+  private static final int deviceIDwheels = 18;
+  private CANSparkMax wheels;
+  private double targetPostiton = .99;
 
+
+  private AbsoluteEncoder ampEncoder;
   /** Creates a new AmpSubsystem. */
   public AmpSubsystem() {
     amp = new CANSparkMax(deviceIDamp, MotorType.kBrushless);
     amp.restoreFactoryDefaults();
+    wheels = new CANSparkMax(deviceIDwheels, MotorType.kBrushless);
+    wheels.restoreFactoryDefaults();
+    ampEncoder = amp.getAbsoluteEncoder(Type.kDutyCycle);
   }
 
   @Override
   public void periodic() {
-    // This method will be called once per scheduler run
+    SmartDashboard.putNumber("amp Position", getPosition());
+    SmartDashboard.putNumber("target position", getTargetPosition());
+  }
+
+  public void setSpeed(double speed) {
+    wheels.set(-speed);
+  }
+ public void setAmpSpeed(double speed) {
+    amp.set(-speed);
+  }
+  public double getPosition(){
+    return ampEncoder.getPosition();
+  }
+
+  public void ampShoot(double speed) {
+    wheels.set(speed);
+  }
+
+  public void setTargetPosition (double targetPostiton){
+    this.targetPostiton = targetPostiton;
+  }
+
+  public double getTargetPosition (){
+    return targetPostiton;
   }
 }
