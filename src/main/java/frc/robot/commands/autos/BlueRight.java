0957b5b2d69be3d355ae27240381621954bd474f
wharrier <wharrier@yahoo.com>
2024-03-17 10:55:19 -0500

    Recovered version.

--- src/main/java/frc/robot/commands/autos/BlueRight.java
+++ src/main/java/frc/robot/commands/autos/BlueRight.java
@@ -12,9 +12,13 @@ import frc.robot.TrajectoryCommandFactory;
 import frc.robot.subsystems.DriveSubsystem;
 import java.util.List;
 
-public class BlueRight {
+public class BlueRight extends GenAutoCommand {
   public static Pose2d StartPose = new Pose2d(0.890000, 4.450000, new Rotation2d(Units.degreesToRadians(-60.000000)));
 
+  @Override
+  public Pose2d getStartingPose() {
+    return StartPose;
+  }
   public static String NAME = "BlueRight";
 
   public static Command buildAuto(DriveSubsystem driveSubsystem, TrajectoryCommandFactory trajectoryCommandFactory) {
@@ -25,7 +29,7 @@ public class BlueRight {
       .andThen(
         CommandRegistry.getCommand("IntakeOn")
         .alongWith(buildDriveOut1(driveSubsystem, trajectoryCommandFactory)))
-      .andThen(CommandRegistry.getCommand("Lift PP"))
+      .andThen(CommandRegistry.getCommand("Lift 35"))
       .andThen(buildDriveIn1(driveSubsystem, trajectoryCommandFactory))
       .andThen(CommandRegistry.getCommand("Shoot"))
       .andThen(
@@ -41,7 +45,7 @@ public static Command buildDriveOut1(DriveSubsystem driveSubsystem, TrajectoryCo
   Trajectory trajectory = trajectoryCommandFactory.createTrajectory(
       new Pose2d(0.8938335058664806, 4.452607704941355,  new Rotation2d(Units.degreesToRadians(-59.99999999999999))),
       new ArrayList<Translation2d>(),
-      new Pose2d(8.528240247683364, 0.7739511397354721, new Rotation2d(Units.degreesToRadians(0.0)))
+      new Pose2d(8.528240247683364, 0.37, new Rotation2d(Units.degreesToRadians(0.0)))
   );
   return trajectoryCommandFactory.createTrajectoryCommand(trajectory);
 }
@@ -50,18 +54,18 @@ public static Command buildDriveIn1(DriveSubsystem driveSubsystem, TrajectoryCom
   Trajectory trajectory = trajectoryCommandFactory.createTrajectory(
       new Pose2d(8.28, 0.79,  new Rotation2d(Units.degreesToRadians(0.0))),
       List.of(
-        new Translation2d(5.70952937304509, 1.5574572133637385)
+        new Translation2d(5.124431127610109, 1.4897778107624486)
       ),
-      new Pose2d(4.0, 2.904705461919659, new Rotation2d(Units.degreesToRadians(-33.0)))
+      new Pose2d(3.4545606733344716, 2.904705461919659, new Rotation2d(Units.degreesToRadians(-47.3532968661083)))
   );
   return trajectoryCommandFactory.createTrajectoryCommand(trajectory);
 }
 
 public static Command buildDriveOut2(DriveSubsystem driveSubsystem, TrajectoryCommandFactory trajectoryCommandFactory) {
   Trajectory trajectory = trajectoryCommandFactory.createTrajectory(
-      new Pose2d(4.0, 2.904705461919659,  new Rotation2d(Units.degreesToRadians(-33.0))),
+      new Pose2d(3.4545606733344716, 2.904705461919659,  new Rotation2d(Units.degreesToRadians(-47.3532968661083))),
       List.of(
-        new Translation2d(6.263716595855327, 1.7676661599469314)
+        new Translation2d(5.523478748697167, 1.3301587623276245)
       ),
       new Pose2d(8.3, 2.436512808166183, new Rotation2d(Units.degreesToRadians(23.025492008528147)))
   );
@@ -72,9 +76,9 @@ public static Command buildDriveIn2(DriveSubsystem driveSubsystem, TrajectoryCom
   Trajectory trajectory = trajectoryCommandFactory.createTrajectory(
       new Pose2d(8.3, 2.436512808166183,  new Rotation2d(Units.degreesToRadians(23.025492008528147))),
       List.of(
-        new Translation2d(5.967513080215373, 1.729446351477261)
+        new Translation2d(5.430367637110186, 1.5163809855015853)
       ),
-      new Pose2d(4.0, 2.904705461919659, new Rotation2d(Units.degreesToRadians(-33.0)))
+      new Pose2d(3.4545606733344716, 2.904705461919659, new Rotation2d(Units.degreesToRadians(-44.49247853130129)))
   );
   return trajectoryCommandFactory.createTrajectoryCommand(trajectory);
 }
